c62e3971c58b97e1291a07dc58e67283c72c4765
Anthony Mortimer <agm20@outlook.com>
2026-09-05 00:54:46 -0400

    Make array size much bigger for testing

--- sequential/arraysum.c
+++ sequential/arraysum.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SIZE 10000
+#define SIZE 1000000000
 
-int sumArray(int arr[], int size)
+long long sumArray(int arr[], int size)
 {
-    int sum = 0;
+    long long sum = 0;
 
     for (int i = 0; i < size; i++)
         sum += arr[i];
@@ -14,14 +15,16 @@ int sumArray(int arr[], int size)
 
 int main()
 {
-    int arr[SIZE];
+    // int arr[SIZE];
+    int *arr = (int*)malloc(SIZE * sizeof(int));
+
     for (int i = 0; i < SIZE; i++)
     {
         arr[i] = i + 1;
     }
     
-    int totalSum = sumArray(arr, SIZE);
-    printf("Total Sum: %d\n", totalSum);
+    long long totalSum = sumArray(arr, SIZE);
+    printf("Total Sum: %lld\n", totalSum);
 
     return 0;
 }
