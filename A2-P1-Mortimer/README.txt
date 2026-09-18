ac3ad735a1ea5708c2b74a9935bf8e02a23ff152
Anthony Mortimer <agm20@outlook.com>
2026-09-17 23:15:32 -0400

    Complete task 1

---
+++ A2-P1-Mortimer/README.txt
@@ -0,0 +1,2 @@
+# Assignment A2-P1 - Anthony Mortimer
+## CSC 410 DT1
--- A2-P1-Mortimer/starwars.c
+++ A2-P1-Mortimer/starwars.c
@@ -14,16 +14,61 @@ int main() {
     // Create 4 child processes - 4 different characters adjusting shield power
 
             // Luke increases the shield power by 25
+            pid = fork();
+
+            if (pid == 0)
+            {
+                printf("Luke: Adjusting shields...\n");
+                shield_power += 25;
+                printf("Luke: Shield power level now at %d%%\n", shield_power);
+                return 0;
+            }
+
             // Han increases the shield power by 20
+            pid = fork();
+
+            if (pid == 0)
+            {
+                printf("Han: Adjusting shields...\n");
+                shield_power += 20;
+                printf("Han: Shield power level now at %d%%\n", shield_power);
+                return 0;
+            }
             // Chewbacca increases the shield power by 30
+            pid = fork();
+
+            if (pid == 0)
+            {
+                printf("Chewbacca: Adjusting shields...\n");
+                shield_power += 30;
+                printf("Chewbacca: Shield power level now at %d%%\n", shield_power);
+                return 0;
+            }
             // Leia increases the shield power by 15
+            pid = fork();
+
+            if (pid == 0)
+            {
+                printf("Leia: Adjusting shields...\n");
+                shield_power += 15;
+                printf("Leia: Shield power level now at %d%%\n", shield_power);
+                return 0;
+            }
                
 
     // Check if process creation failed
-        // -----> Write you code here 
-
-    // Make parent process wait for all child processes to complete
-        // -----> Write you code here 
+    if (pid == -1)
+    {
+        printf("Fork failed.");
+        return -1;
+    }
+    
+    // Make parent process wait for all child processes to complete 
+    for (int i = 0; i < 4; i++)
+    {
+        wait(NULL);
+    }
+    
 
 
     // Parent process reports final state
