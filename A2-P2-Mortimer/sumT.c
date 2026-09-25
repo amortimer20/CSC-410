657f19675aa840eb0a16efbf58d8de658b2f271b
Anthony Mortimer <agm20@outlook.com>
2026-09-25 11:40:55 -0400

    Complete task 1

--- A2-P2-Mortimer/sumT.c
+++ A2-P2-Mortimer/sumT.c
@@ -7,17 +7,32 @@
 #define NUM_THREADS 4
 
 long long arr[SIZE];
-int partialSums[NUM_THREADS] = {0}; // Array to store partial sums for each thread
+long long partialSums[NUM_THREADS] = {0}; // Array to store partial sums for each thread
+long workSize = SIZE / NUM_THREADS;
+
 
 // Entry function for each thread
 void* sumPart(void* arg) 
 {
     // Divide the work for each thread based on their id and let them compute partial sums
-        // -----> Write your code here
+
+    // Last thread goes to the end of the array
+    int id = *(int *)arg;
+    long startIndex = id * workSize;   
+    long endIndex = id == NUM_THREADS - 1 ? SIZE - 1 : startIndex + workSize;
+    long long sum = 0;
+
+    for (int i = startIndex; i < endIndex; i++)
+    {
+        sum += arr[i];
+    }
+
+    partialSums[id] = sum;
 }
 
 int main() 
 {
+
     // Initialize the array
     for (int i = 0; i < SIZE; i++) {
         arr[i] = i + 1; 
@@ -27,10 +42,18 @@ int main()
     int thread_ids[NUM_THREADS];
 
     // Create threads to compute partial sums
-        // ------> Write your code here
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, sumPart, &thread_ids[i]);
+    }
 
     // Wait for all threads to finish
-        // -------> Write your code here
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    
 
     // Combine the partial sums from all threads
     long long totalSum = 0;
